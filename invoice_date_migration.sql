0b11e9bcb32abc02baedc667963a858802703d6f
musaver <musaverleo@gmail.com>
2025-08-16 17:11:49 +0500

    Add all files

--- invoice_date_migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration to add invoice_date column to orders table
-
--- Add invoice_date column to orders table
-ALTER TABLE orders 
-ADD COLUMN invoice_date DATETIME;
--- product_description_migration.sql
+++
@@ -1,5 +0,0 @@
--- Migration to add product_description column to order_items table
-
--- Add product_description column to order_items table
-ALTER TABLE order_items 
-ADD COLUMN product_description TEXT;
